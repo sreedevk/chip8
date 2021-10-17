c6c9e913e723e10e6bd90add9e2c025d783af11f
sreedev <sreedevpadmakumar@gmail.com>
2021-10-17 17:39:52 +0530

    added sample assembly

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,11 +1,18 @@
+use std::{env, fs};
 pub struct Assembler;
 
 impl Assembler {
     pub fn assemble(program: String) {
-        println!("STARTING CHIP8 ASSEMBLER: {}", program);
+        let code = Assembler::read(program);
+        println!("code: {}", code);
     }
 
     pub fn lex(line: String) {
         println!("{:#?}", line.split_whitespace());
+    }
+
+    pub fn read(program: String) -> String {
+        fs::read_to_string(program.as_str())
+            .expect("Couldn't Load Application")
     }
 }
